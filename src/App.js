0d6c80e2067f428b374f9e2da9629a6e6adf8695
Oleg Stakhiv <veishen19@gmail.com>
2017-07-03 08:37:00 +0000

    beginning of create functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Router, browserHistory, Route, IndexRedirect, withRouter } from 'react-router'
 import List from './containers/blogPosts/list';
+import Create from './containers/blogPosts/create';
 import NotFoundPage from './components/NotFoundPage';
 
 export default class App extends Component {
@@ -12,6 +13,7 @@ export default class App extends Component {
                     <IndexRedirect to="/posts"/>
                 </Route>
                 <Route path="/posts" component={List} />
+                <Route path="/posts/create" component={Create} />
                 <Route path="*" component={NotFoundPage} />
             </Router>
         );
---
+++ src/containers/blogPosts/create.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+export default class Create extends Component {
+
+    handleSubmit(data) {
+        console.log('form submission data', data);
+    }
+
+    render() {
+        return (
+            <div>
+                <form name="blog_post" className="form-horizontal" onSubmit={this.handleSubmit}>
+                    <div id="blog_post">
+                        <div className="form-group">
+                            <label className="col-sm-2 control-label required" htmlFor="blog_post_title">Title</label>
+                            <div className="col-sm-10">
+                                <input type="text"
+                                       id="blog_post_title"
+                                       required="required"
+                                       className="form-control"/>
+                            </div>
+                        </div>
+                        <div className="form-group">
+                            <label className="col-sm-2 control-label required" htmlFor="blog_post_body">Body</label>
+                            <div className="col-sm-10">
+                                <input type="text"
+                                       id="blog_post_body"
+                                       required="required"
+                                       className="form-control"/>
+                            </div>
+                        </div>
+                        <div className="form-group">
+                            <div className="col-sm-2"></div>
+                            <div className="col-sm-10">
+                                <button type="submit"
+                                        id="blog_post_submit"
+                                        className="btn-default btn">
+                                    Submit
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+
+
+
+
+}
