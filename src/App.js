d8301569e27ae07279aef67cdb6f1e0dc920b6df
Oleg Stakhiv <veishen19@gmail.com>
2017-07-03 08:23:54 +0000

    add routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
+import { Router, browserHistory, Route, IndexRedirect, withRouter } from 'react-router'
 import List from './containers/blogPosts/list';
+import NotFoundPage from './components/NotFoundPage';
 
 export default class App extends Component {
 
     render() {
         return (
-            <div>
-                <h1>Hello, world!</h1>
-                <List></List>
-            </div>
+            <Router history={browserHistory}>
+                <Route path="/" >
+                    <IndexRedirect to="/posts"/>
+                </Route>
+                <Route path="/posts" component={List} />
+                <Route path="*" component={NotFoundPage} />
+            </Router>
         );
       }
 }
