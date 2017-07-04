69b92ad703ac2da536a7cbbb95162c2b5308ff67
Oleg Stakhiv <veishen19@gmail.com>
2017-07-04 07:24:06 +0000

    create functionality

--- src/actions/blogPostActions.js
+++ src/actions/blogPostActions.js
@@ -15,3 +15,16 @@ export function fetchBlogPosts() {
     }).then(res => res.json())
         .catch(err => err);
 }
+
+export function createBlogPost(data) {
+    return fetch('http://sa/web/app_dev.php/post', {
+        method: 'POST',
+        mode: 'CORS',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(res => {
+        return res;
+    }).catch(err => err)
+}
